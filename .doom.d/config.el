7b46e0fc1f630c8b556a8098973594d12c594e22
Kaka <kaka@gini.co>
2019-09-23 13:11:00 +0800

    Added .doom.d.

---
+++ .doom.d/config.el
@@ -0,0 +1,21 @@
+;; Place your private configuration here
+
+(setq display-line-numbers-type 'relative)
+(setq show-trailing-whitespace t)
+
+(setq doom-font (font-spec :family "Fira Code" :size 13))
+
+(add-hook! 'js2-mode-hook 'eslintd-fix-mode)
+
+(add-hook! 'before-save-hook
+           'delete-trailing-whitespace
+           'doom/delete-trailing-newlines)
+
+(setq ispell-extra-args '("--sug-mode=ultra" "--lang=en" "--run-together")) ;; --camel-case on 0.60.8
+
+(defun str-camel-case (beg end)
+  "some words => someWords"
+  (interactive "r")
+  (let ((s (buffer-substring beg end)))
+    (delete-region beg end)
+    (insert (s-lower-camel-case s))))
